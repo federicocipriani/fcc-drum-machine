33651468035cfaa7a6f80c10ec66819410bfa107
Federico <64685113+federicocipriani@users.noreply.github.com>
2020-06-21 23:42:58 +0100

    almost-finished-210620

--- script.js
+++ script.js
@@ -1,68 +1,52 @@
 const pads = document.getElementById('pad-container');
+const ids = document.querySelectorAll('audio');
+const display = document.getElementById('display');
 
-document.addEventListener("keydown", function(e) {
-    console.log(e);
-    var key = e.key.toUpperCase();
-    switch(key) {
-        case 'Q':
-            document.getElementById(key).play();
-            break;
-        case 'W':
-            document.getElementById(key).play();
-            break;
-        case 'E':
-            document.getElementById(key).play();
-            break;
-        case 'A':
-            document.getElementById(key).play();
-            break;
-        case 'S':
-            document.getElementById(key).play();
-            break;
-        case 'D':
-            document.getElementById(key).play();
-            break;
-        case 'Z':
-            document.getElementById(key).play();
-            break;
-        case 'X':
-            document.getElementById(key).play();
-            break;
-        case 'C':
-            document.getElementById(key).play();
-            break;
-    }
-});
+loadEventListener();
 
-pads.addEventListener('click', (event) => {
-    console.log(event.target.lastElementChild.id);
-    switch(event.target.lastElementChild.id) {
-        case 'Q':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'W':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'E':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'A':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'S':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'D':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'Z':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'X':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
-        case 'C':
-            document.getElementById(event.target.lastElementChild.id).play();
-            break;
+function loadEventListener() {
+
+    pads.addEventListener('click', playSoundClick);
+    document.addEventListener('keydown', playSoundKeyboard);
+
+}
+
+function playSoundClick(e) {
+    let sound = e.target.children[0];
+    var playPromise = sound.play();
+    if (playPromise !== undefined) {
+        playPromise.then(_ => {
+          // Automatic playback started!
+          // Show playing UI.
+        })
+        .catch(error => {
+          // Auto-play was prevented
+          // Show paused UI.
+        });
     }
-});
+    displaySound(sound);
+}
+
+function playSoundKeyboard(e) {
+    ids.forEach(item => {
+        if (item.id === e.key.toUpperCase()) {
+            let sound = document.getElementById(e.key.toUpperCase());
+            var playPromise = sound.play();
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                  // Automatic playback started!
+                  // Show playing UI.
+                })
+                .catch(error => {
+                  // Auto-play was prevented
+                  // Show paused UI.
+                });
+            }
+            displaySound(sound)
+        }
+    })
+}
+
+function displaySound(sound) {
+    display.innerText = sound.parentElement.id;
+}
